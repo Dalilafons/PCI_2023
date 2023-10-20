0cfebeab63bc737a794644d81fe90711bfdb8b80
Dalilafons <142357126+Dalilafons@users.noreply.github.com>
2023-10-19 23:35:06 -0600

    Entrega final
    Esta es la entrega final del programa.

--- calculadora_calorias.py
+++ calculadora_calorias.py
@@ -1,51 +1,61 @@
 # Variables individuales para cada alimento.
-ensalada = 15
-sopa = 18
-pasta = 20
-pollo = 24
-pescado = 27
-arroz = 30
-carne = 36
-patatas = 38
-legumbres = 40
-verduras = 42
-huevo = 45
-tostada = 34
-bacon = 23
-salchichas = 45
-manzana = 10
-pera = 8
-platano = 7
-cereales = 9
-yogur = 11
+ENSALADA = 15
+SOPA = 18
+PASTA = 20
+POLLO = 24
+PESCADO = 27
+ARROZ = 30
+CARNE = 36
+PATATAS = 38
+LEGUMBRES = 40
+VERDURAS = 42
+HUEVO = 45
+TOSTADA = 34
+BACON = 23
+SALCHICHAS = 45
+MANZANA = 10
+PERA = 8
+PLATANO = 7
+CEREALES = 9
+YOGUR = 11
 
 # Listas anidadas para desayunos, comidas, y cenas.
 desayunos = [
-    [manzana, pera, platano],       # ligero
-    [tostada, cereales, yogur],    # medio
-    [huevo, bacon, salchichas]     # pesado
+    [MANZANA, PERA, PLATANO],      # ligero
+    [TOSTADA, CEREALES, YOGUR],    # medio
+    [HUEVO, BACON, SALCHICHAS]     # pesado
 ]
 
 comidas = [
-    [ensalada, sopa, pasta],       # ligero
-    [pollo, pescado, arroz],       # medio
-    [carne, patatas, legumbres]    # pesado
+    [ENSALADA, SOPA, PASTA],       # ligero
+    [POLLO, PESCADO, ARROZ],       # medio
+    [CARNE, PATATAS, LEGUMBRES]    # pesado
 ]
 
 cenas = [
-    [sopa, verduras, pescado],     # ligero
-    [pollo, pescado, arroz],       # medio
-    [carne, patatas, legumbres]    # pesado
+    [SOPA, VERDURAS, PESCADO],     # ligero
+    [POLLO, PESCADO, ARROZ],       # medio
+    [CARNE, PATATAS, LEGUMBRES]    # pesado
 ]
 
+# Para más detalles sobre las listas en Python, 
+# consulte: https://docs.python.org/3/tutorial/introduction.html#lists
+
+
 def mostrar_menu_principal():
+    """Muestra el menu principal y toma la opcion del usuario.
+
+    Return:
+        int: Opcion seleccionada por el usuario.
+    """
     print("Menu principal:\n1.Desayuno\n2.Comida\n3.Cena\n4.Salir")
-    opcion = int(input("Elije una opción: "))
+    opcion = int(input("Elije una opcion: "))
     return opcion
 
 def mostrar_comida(tipo_comida):
     """
-    Esta función recibe un numero 1-3  e imprime el tipo de comida al que corrresponde.
+    Esta funcion recibe un numero 1-3  e 
+    imprime el tipo de comida al que corrresponde.
     Si recibe 1, imprime "Elegiste Desayuno".
     Si recibe 2, imprime "Elegiste Comida".
     Si recibe 3, imprime "Elegiste Cena".
@@ -56,8 +66,8 @@ def mostrar_comida(tipo_comida):
     Return:
         None
     """
-    #Validar entrada
-    if ((tipo_comida >=1) and (tipo_comida <=3)):
+    # Validar entrada
+    if ((tipo_comida >= 1) and (tipo_comida <= 3)):
         if tipo_comida == 1:
             print("Elegiste Desayuno...")
         elif tipo_comida == 2:
@@ -69,7 +79,8 @@ def mostrar_comida(tipo_comida):
 
 def mostrar_grupos_alimentos(tipo_comida):
     """
-    Esta función recibe un numero 1-3  e imprime los alimentos si es ligera, media o pesada.
+    Esta funcion recibe un numero 1-3  e imprime 
+    los alimentos si es ligera, media o pesada.
     Si recibe 1, imprime:
     Ligera: Manzana, Pera, Plátano
     Media: Tostada, Cereales, Yogur
@@ -81,17 +92,17 @@ def mostrar_grupos_alimentos(tipo_comida):
     Return:
         None
     """
-    #Validar entrada
-    if ((tipo_comida>=1) and (tipo_comida<=3)):
+    # Validar entrada
+    if ((tipo_comida >= 1) and (tipo_comida <= 3)):
         if tipo_comida == 1:  # Desayuno
-            print("Ligera: Manzana, Pera, Plátano")
+            print("Ligera: Manzana, Pera, Platano")
             print("Media: Tostada, Cereales, Yogur")
             print("Pesada: Huevo, Bacon, Salchichas")
         elif tipo_comida == 2:  # Comida
             print("Ligera: Ensalada, Sopa, Pasta")
             print("Media: Pollo, Pescado, Arroz")
             print("Pesada: Carne, Patatas, Legumbres")
-        else:#Cena
+        else:  # Cena
             print("Ligera: Sopa, Verduras, Pescado")
             print("Media: Pollo, Pescado, Arroz")
             print("Pesada: Carne, Patatas, Legumbres")
@@ -99,13 +110,20 @@ def mostrar_grupos_alimentos(tipo_comida):
         print("Error, el valor que ingresaste no existe.")
 
 def mostrar_ligera_media_pesada():
-    print("Menú de opciones:\n1.Ligera\n2.Media\n3.Pesada")
+    """Muestra el menu para elegir el tipo de comida 
+    y toma la opcion del usuario.
+
+    Return:
+        int: Opcion seleccionada por el usuario.
+    """
+    print("Menu de opciones:\n1.Ligera\n2.Media\n3.Pesada")
     opcion = int(input("Elije una opción: "))
     return opcion
 
 def mostrar_opcion_ligera_media_pesada(opcion_comida):
     """
-    Esta función recibe un numero 1-3  e imprime si elegiste ligera, media o pesada.
+    Esta funcion recibe un numero 1-3  
+    e imprime si elegiste ligera, media o pesada.
     Si elegiste 1, imprime "Elegiste ligera"
     Si elegiste 2, imprime "Elegiste Media"
     Si elegiste 3, imprime "Elegiste Pesada"
@@ -116,8 +134,8 @@ def mostrar_opcion_ligera_media_pesada(opcion_comida):
     Return:
         None
     """
-    #Validar entrada
-    if ((opcion_comida >= 1) and (opcion_comida <=3)):
+    # Validar entrada
+    if ((opcion_comida >= 1) and (opcion_comida <= 3)):
         if opcion_comida == 1:
             print("Elegiste Ligera...")
         elif opcion_comida == 2:
@@ -127,9 +145,11 @@ def mostrar_opcion_ligera_media_pesada(opcion_comida):
     else:
         print("Error, el valor que ingresaste no existe.")
 
-def calcular_calorias(tipo_comida, opcion_comida):
+def calcular_cal(tipo_comida, opcion_comida):
     """
-    Esta función recibe tipo de comida 1-3 y opcion de comida 1-3.
+    Esta funcion recibe tipo de comida 1-3 y 
+    opcion de comida 1-3.
+
     Si el tipo de comida es 1 y opcion de comida es 1.
 
     Args:
@@ -139,13 +159,17 @@ def calcular_calorias(tipo_comida, opcion_comida):
     Return:
         ligero_desayuno
     """
-    #Validar entrada
-    if ((tipo_comida>=1) and (tipo_comida<=3)):
+    # Validar entrada
+    if ((tipo_comida >= 1) and (tipo_comida <= 3)):
         comidas_dict = {
         1: desayunos,
         2: comidas,
         3: cenas
         }
+
+        # Para más detalles sobre diccionarios, 
+        # https://docs.python.org/3/tutorial/datastructures.html#dictionaries
+
         comidas_elegidas = comidas_dict[tipo_comida]
         total = sum(comidas_elegidas[opcion_comida - 1])
         return total
@@ -157,70 +181,73 @@ if __name__ == '__main__':
     total_calorias = 0
     contador_comidas = 0
 
-    #Bievenida
+    # Bievenida
     while True:
         
-        #Mostrar menu
-        #Ejemplo: 1. Desayuno
-
+        # Mostrar menu
         comida_principal = mostrar_menu_principal()
         print('\n')
 
-        #Salir si el usuario elige 4 o un numero diferente de 1,2 o 3.
+        # Salir si el usuario elige 4 o un numero diferente de 1,2 o 3.
         if (comida_principal == 4):
                 print("Elegiste salir")
                 break
-        while((comida_principal >= 4) or (comida_principal <1 )):
+        
+        while((comida_principal >= 4) or (comida_principal < 1)):
                 print("Debes de elegir, desayuno, comida o cena!!!")
                 comida_principal= mostrar_menu_principal()
        
-        #Mostrar comida (Desayuno, Comida, Cena)
+        # Mostrar comida (Desayuno, Comida, Cena)
         # Ejemplo: Elegiste Desayuno...
         print("*" * 50)
         mostrar_comida(comida_principal)
 
-        #Mostrar tipos de alimentos indicados por comida (Desayuno, Comida, Cena)
-        #Ejemplo: Ligera: Manzana, Pera, Plátano
+        # Mostrar tipos de alimentos indicados
+        #  por comida (Desayuno, Comida, Cena)
+        # Ejemplo: Ligera: Manzana, Pera, Plátano
         mostrar_grupos_alimentos(comida_principal)
         print("*" * 50)
 
-        #Mostrar opcion de acuerdo a comida (desayuno, comida, cena) y tipo (ligera, media, pesada)
+        # Mostrar opcion de acuerdo a comida 
+        # (desayuno, comida, cena) y tipo (ligera, media, pesada)
         # Ejemplo: Elegiste Ligera... 
         # eleccion = mostrar_ligera_media_pesada()
         opcion_comida = mostrar_ligera_media_pesada()
         print("*" * 50)
+
         while((opcion_comida >= 4) or (opcion_comida < 1)):
             print("Debes de elegir, ligera, media o pesada!!!")
             opcion_comida = mostrar_ligera_media_pesada()
         
-        #Mostrar opcion de la comida que elegiste
+        # Mostrar opcion de la comida que elegiste
         mostrar_opcion_ligera_media_pesada(opcion_comida)
     
-        #Mostrar total de comidas
-        if (opcion_comida>=1 and opcion_comida<=3):
+        # Mostrar total de comidas
+        if (opcion_comida >= 1 and opcion_comida <= 3):
             contador_comidas += 1
-            total_calorias += calcular_calorias(comida_principal, opcion_comida)
-            #Calcular calorias de acuerdo a opcion de comida y tipo de comida
-            #Ejemplo: Calorías totales: 180
+            total_calorias += calcular_cal(comida_principal, opcion_comida)
+            # Calcular calorias de acuerdo a opcion de comida 
+            # y tipo de comida.
+            # Ejemplo: Calorías totales: 180
         print(f"Has comido {contador_comidas} veces")
         
-        #Mensajes preventivos de acuerdo a numero de comidas
+        # Mensajes preventivos de acuerdo a numero de comidas
         if contador_comidas < 3:
-            print("Debes comer al menos 3 veces al día")
+            print("Debes comer al menos 3 veces al dia")
         elif contador_comidas == 3:
-            print("Número de comidas recomendadas")
+            print("Numero de comidas recomendadas")
             #  Reducir calorías en 20%
             total_calorias *= 0.8
         else:
             # Aumentar calorías en 10%
             total_calorias *= 1.1
-            print("Advertencia, número de comidas mayor a 3")
+            print("Advertencia, numero de comidas mayor a 3")
 
-        print(f"Total de calorías: {total_calorias}")
+        print(f"Total de calorias: {total_calorias}")
         print("*" * 50)
         print('\n')
-    #Mostrar si eleecion es saludable o excede las calorias recomendadas
+    # Mostrar si eleecion es saludable o excede las calorias recomendadas
     if  total_calorias > 0 and  total_calorias <= 1500:
-        print("Tu elección es saludable")
+        print("Tu eleccion es saludable")
     elif total_calorias > 1500:
-        print("Elección excede calorías recomendadas")
+        print("Eleccion excede calorias recomendadas")
